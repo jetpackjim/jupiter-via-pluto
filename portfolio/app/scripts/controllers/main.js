3fdf56c74ba6e8911b3f3ba759f7c3bddfc1a856
jetpackjim <amorris.home@gmail.com>
2014-04-24 16:06:02 +0100

    1st pass alienworld portfolio website

--- portfolio/app/scripts/controllers/main.js
+++ portfolio/app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('portfolioApp')
+var portfolioApp = angular.module('portfolioApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
@@ -8,3 +8,42 @@ angular.module('portfolioApp')
       'Karma'
     ];
   });
+
+
+ // routes configuration
+portfolioApp.config(function($routeProvider){
+	$routeProvider
+	.when('/about',{
+		templateUrl: 'views/about.html',
+		//controller: 'spAboutController'
+	})
+	.when('/skills',{
+		templateUrl: 'views/skills.html',
+		//controller: 'spContactController'
+	})
+	.when('/work',{
+		templateUrl: 'views/work.html',
+		//controller: 'spContactController'
+	})
+	.when('/fun',{
+		templateUrl: 'views/fun.html',
+		//controller: 'spContactController'
+	})
+	.when('/contact',{
+		templateUrl: 'views/contact.html',
+		//controller: 'spContactController'
+	})
+	.otherwise({
+		redirectTo: '/about',
+		
+	});
+	
+	
+});
+
+function NavbarController($scope, $location)
+{
+	$scope.isActive = function(viewLocation) {
+		return viewLocation == $location.path();
+	}
+}
