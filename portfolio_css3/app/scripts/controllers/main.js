2ed3c4c8a8f129cbaa26a5ee14080b01259b70ce
jetpackjim <amorris.home@gmail.com>
2014-05-10 16:09:06 +0100

    created new version of portfolio project that will use css and sprite sheets instead of svg

---
+++ portfolio_css3/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('portfolioCss3App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
